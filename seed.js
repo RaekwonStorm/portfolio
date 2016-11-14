8592ca0258fc1802a592281617326195b512a803
Raekwon <RaekwonStorm@users.noreply.github.com>
2016-11-14 13:40:21 -0500

    Update seed.js

--- seed.js
+++ seed.js
@@ -11,19 +11,19 @@ let pics = [
   description: "Black Dragon Pool, with Jade Dragon Snow Mountain in the background",
   chinese: "黑龍潭, 背景就是玉龍雪山",
   location: "Lijiang，Yunnan；雲南麗江",
-  imgUrl: "/pictures/IMG_4251.jpg"
+  imgUrl: "/pictures/IMG_4251.JPG"
 },
 {
   description: "Terraced farms, Naxi minority mountain village in Tiger Leaping Gorge",
   chinese: "梯田農場在虎跳峽的納西族山村",
   location: "Tiger Leaping Gorge，Yunnan；雲南虎跳峽",
-  imgUrl: "/pictures/IMG_4289.jpg"
+  imgUrl: "/pictures/IMG_4289.JPG"
 },
 {
   description: "Mount Cook，South Island NZ",
   chinese: "Cook山，，紐西蘭南島",
   location: "Mount Cook，South Island NZ；Cook山，，紐西蘭南島",
-  imgUrl: "/pictures/IMG_1043.jpg"
+  imgUrl: "/pictures/IMG_1043.JPG"
 }
 ]
 
