04825f6edd55a227f31127e1a86b770f275b5ac1
Reed Branson <reed.branson@gmail.com>
2016-11-14 19:49:38 -0500

    replaced fetch with axios

--- browser/react/enter-hooks.js
+++ browser/react/enter-hooks.js
@@ -1,12 +1,13 @@
 'use strict';
 
+import axios from 'axios';
 import store from './store';
 import {SetPicturesToState} from './action-creators/Pictures';
 
 export const onTravelEnter = function () {
   if (!store.getState().pictures.length) {
-    fetch('api/')
-    .then(pictures => pictures.json())
+    axios.get('api/')
+    .then(pictures => pictures.data)
     .then(pictures => store.dispatch(SetPicturesToState(pictures)));
   }
 };
